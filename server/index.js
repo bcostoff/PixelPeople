30ac45064a0532e1931f07caa8a6a848e6c413cd
Beau Costoff <wbeaumont@rogers.com>
2022-04-11 12:23:41 -0400

    readying for deployment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 // server/index.js
-
-const express = require("express");
+const path = require('path');
+const express = require('express');
 var mysql = require('mysql');
 var fs = require('fs');
 
@@ -19,6 +19,10 @@ con.connect(function(err) {
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/person", (req, res) => {
     var obj;
@@ -95,6 +99,10 @@ app.post('/guess', (req, res) => {
     // });
 })
 
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`); 
